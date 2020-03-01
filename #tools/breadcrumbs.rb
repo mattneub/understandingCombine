10a833be8d155ae3af94154c6b4dd1443228d538
Matt Neuburg <matt@tidbits.com>
2020-02-29 17:57:40 -0800

    finished publishers section, worked out breadcrumbs and other navigation, heading; could actually release at this point

---
+++ #tools/breadcrumbs.rb
@@ -0,0 +1,33 @@
+def breadcrumbs()
+  nextprevs = @adrPageTable['nextprevs'].read
+  doc = Opml.textToOpml(nextprevs)
+  # first question is: where am I
+  me = adrObject.simplename
+  toc = (me.to_s == "toc")
+  thisnode = doc.search("//outline[@text='#{me}']").first # nil for toc, it's not in the nextprevs!
+  # start at the top
+  top = doc.root
+  body = top.first_element_child
+  # do children
+  s = "<div class='breadcrumbs'>"
+  s = "<div class='toc'>" if toc
+  s << doOneLevel(body, thisnode)
+  s << "</div>"
+  s << "<div style='clear:both'></div>"
+  s
+end
+def doOneLevel(node, thisnode)
+  s = ""
+  node.children.each do |line|
+    name = line["text"]
+    title, path = html.getTitleAndPaths(name)
+    link = html.getLink(title, path)
+    s << "<p>" + link + "</p>\n"
+    if line.children.count > 0 && (thisnode.nil? || thisnode == line || thisnode.ancestors.include?(line))
+      s << "<div style='padding-left:5em;'>\n"
+      s << doOneLevel(line, thisnode)
+      s << "</div>\n"
+    end
+  end
+  s
+end
--- #tools/nextprevlinks.rb
+++ #tools/nextprevlinks.rb
@@ -2,9 +2,9 @@ def nextprevlinks()
   p, n = html.getNextPrev(adrObject)
   ntitle, npath = html.getTitleAndPaths(n) if n
   ptitle, ppath = html.getTitleAndPaths(p) if p
-  s = ""
-  s << "Prev: " + html.getLink(ptitle, ptitle) if p
-  s << " | " if p and n
-  s << "Next: " + html.getLink(ntitle, ntitle) if n
-  "<p>#{s}</p>\n"
+  prev = ""
+  prev = "Prev: " + html.getLink(ptitle, ppath) if p
+  nxt = ""
+  nxt = "Next: " + html.getLink(ntitle, npath) if n
+  "<p style='float:left'>#{prev}</p><p style='float:right'>#{nxt}</p>\n<div style='clear:both' />\n"
 end
