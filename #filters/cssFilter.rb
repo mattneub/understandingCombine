41a09e3640eef3b6a1df5630c2df80e47c6bf439
Matt Neuburg <matt@tidbits.com>
2020-03-19 09:44:55 -0700

    improve statement of what map and scan do; starting code cleanup

--- #filters/cssFilter.rb
+++ #filters/cssFilter.rb
@@ -1,6 +1,4 @@
 def cssFilter(adrPageTable)
   # support for SASS
-  if (adrPageTable[:sheetName] == "s2") # this is our only SASS stylesheet; of course you could use some other means of identification
-    adrPageTable[:csstext] = Sass::Engine.new(adrPageTable[:csstext], :syntax => :scss, :style => :expanded).render
-  end
+  adrPageTable[:csstext] = Sass::Engine.new(adrPageTable[:csstext], :syntax => :scss, :style => :expanded).render
 end
